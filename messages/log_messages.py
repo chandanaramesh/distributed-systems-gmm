9e9ad8a2c2aa818765338df83fc6adf5f0855588
Arun Thundyill Saseendran <ats0stv@gmail.com>
2019-04-08 01:16:14 +0100

    Updated the message type in log messages model

--- messages/log_messages.py
+++ messages/log_messages.py
@@ -5,11 +5,14 @@ Created on Thu Apr  4 14:10:48 2019
 @author: manda
 """
 import logging
+from messages.base_message import BaseMessage
 logger = logging.getLogger(__name__)
+
+
 
 class LogEntry(object):
 
-    def __init__(self, term, command, addr, uuid, _type = 0):
+    def __init__(self, term, command, addr, uuid, _type = BaseMessage.AppendEntries):
         self.term = term
         self.command = command
         self.uuid = uuid
