b4f0cc675db478a3eec91667b0721ca89aaf4427
mandas <mandas@tcd.ie>
2019-04-14 14:33:43 +0100

    removed changeConfig

--- messages/config_messages.py
+++ messages/config_messages.py
@@ -19,12 +19,3 @@ class ServerConfig(object):
         self.peers = peers
         logger.debug('Server Configuration: poolsize = {}, currentTerm = {}, votedFor = {}. log = {}, peers = {}'.format(groupInfo, currentTerm, votedFor, log, peers))
         # self.new_quorom = new_quorom
-
-class ConfigChange(object):
-    def __init__(self, new_config, uuid, phase, addr=None):
-        self.new_config = new_config
-        self.uuid = uuid
-        self.addr = addr
-        self.phase = phase
-        self.type = BaseMessage.ChangeMessage
-        logger.debug('Config Change: new_config = {}, uuid = {}, phase = {}, addr = {}'.format(new_config, uuid, phase, addr))
